3c663033549b0964a37caaf2a09e81b8e92e2bd8
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-07-14 01:35:56 +0530

    Reverse a number

--- Java/practice/Loops and numbers/ReverseNumber.java
+++ Java/practice/Loops and numbers/ReverseNumber.java
@@ -20,3 +20,47 @@ public class ReverseNumber {
         sc.close();
     }
 }
+
+
+
+// MUJ Java assignment 1 solution
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int num = sc.nextInt();
+         
+            // Handle negative numbers
+            boolean isNegative = num < 0;
+            num = Math.abs(num);
+
+            int reversed = 0;
+
+            while (num !=0) {
+                //Check for potential integer overflow
+                if (reversed > (Integer.MAX_VALUE/10) || 
+                    (reversed == (Integer.MAX_VALUE/10) && (num % 10) > 7)) {
+                    System.out.println(0);
+                    return;
+                }
+
+                int digit = num % 10;
+                reversed = reversed * 10 + digit;
+                num /= 10;
+            }	
+
+            //Restore negative sign if original number is negative
+            reversed = isNegative ? -reversed : reversed;
+            System.out.println(reversed);
+        } catch(Exception e) {
+            System.out.println(0); //Return 0 for invalid inputs
+        } finally {
+            sc.close();
+        }
+
+    }
+	
+}
+
