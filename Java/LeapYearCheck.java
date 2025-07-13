71d478481fd52a0411ee319278a2cf38ecdb6126
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-07-14 01:34:31 +0530

    Java program to check whether a year is a leap year or not

--- Java/LeapYearCheck.java
+++ Java/LeapYearCheck.java
@@ -15,9 +15,9 @@ public class LeapYearCheck {
         
         // Check whether the year is a leap year
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            System.out.println(year + " is a Leap Year.");
+            System.out.println(" is a leap Year.");
         } else {
-            System.out.println(year + " is NOT a Leap Year.");
+            System.out.println("not a leap year.");
         }
 
         // Close the scanner object
