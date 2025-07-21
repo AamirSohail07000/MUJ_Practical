606a622ee99e24f24c3d2a51e33d68c302eeead6
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-07-21 23:46:01 +0530

    Largest and Smallest Number in an Array

---
+++ Java/practice/Array Programs/MinMaxInArray.java
@@ -0,0 +1,35 @@
+// Find the Largest and Smallest Number in an Array
+
+import java.util.Scanner;
+
+public class MinMaxInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input size
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];
+        int min = arr[0];
+
+        // Find max and min
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+            if (arr[i] < min)
+                min = arr[i];
+        }
+
+        System.out.println("Largest element: " + max);
+        System.out.println("Smallest element: " + min);
+
+        sc.close();
+    }
+}
