fc93e11b70ccadc1d6a4d695fcfea1724e60cf87
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-08-07 23:20:51 +0530

    Factorial of Numbers in a Range

---
+++ DSA/Recursion_&_Backtracking/factorial3.cpp
@@ -0,0 +1,16 @@
+// Factorial of Numbers in a Range
+
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
+}
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        cout << i << "! = " << factorial(i) << endl;
+    }
+    return 0;
+}
