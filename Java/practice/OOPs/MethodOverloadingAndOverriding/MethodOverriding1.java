f503f622722ff7666b9750cad9b150b16af6827b
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-08-27 00:04:26 +0530

    Method Overriding – Animal Sounds

---
+++ Java/practice/OOPs/MethodOverloadingAndOverriding/MethodOverriding1.java
@@ -0,0 +1,33 @@
+// Method Overriding – Animal Sounds
+
+// Problem:
+// Create a base class Animal with method sound(). Override it in Dog and Cat classes.
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class MethodOverriding1 {
+    public static void main(String[] args) {
+        Animal a1 = new Dog();
+        Animal a2 = new Cat();
+        a1.sound();
+        a2.sound();
+    }
+}
