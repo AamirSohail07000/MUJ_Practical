f781be246f6a363819dd995fc8c03e9d45f674db
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-09-04 04:11:58 +0530

    find the maximum in each sliding window

---
+++ DSA/Stacks_&_Queues/SlidingWindow1.cpp
@@ -0,0 +1,36 @@
+// Given an array nums = [1,3,-1,-3,5,3,6,7] and window size k = 3, find the maximum in each sliding window.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> slidingWindowMax(vector<int>& nums, int k) {
+    deque<int> dq;
+    vector<int> result;
+    
+    for (int i = 0; i < nums.size(); i++) {
+        // Remove elements outside window
+        if (!dq.empty() && dq.front() <= i - k)
+            dq.pop_front();
+        
+        // Remove smaller elements from back
+        while (!dq.empty() && nums[dq.back()] < nums[i])
+            dq.pop_back();
+        
+        dq.push_back(i);
+        
+        // Store max for current window
+        if (i >= k - 1)
+            result.push_back(nums[dq.front()]);
+    }
+    return result;
+}
+
+int main() {
+    vector<int> nums = {1,3,-1,-3,5,3,6,7};
+    int k = 3;
+    vector<int> ans = slidingWindowMax(nums, k);
+    for (int x : ans) cout << x << " ";
+    return 0;
+}
+
